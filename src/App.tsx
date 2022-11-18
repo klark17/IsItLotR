893e1387493bfd1ae1f70015891555490157a846
Kam <kamerynlarkins@yahoo.com>
2022-11-17 23:04:15 -0500

    fixed gh-pages push

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import HomePage from './components/HomePage'
+
+const App: React.FC = () => (
+  <div className="App">
+    <HomePage />
+  </div>
+)
+
+export default App
