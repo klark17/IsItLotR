0ca06d9f0d643c785cfd10393b9f824edbdb7f2a
Kam <kamerynlarkins@yahoo.com>
2022-11-17 22:35:24 -0500

    Updates

--- src/components/ErrorMessage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ErrorMessage: React.FC = () => (
-  <h1>An error occurred.</h1>
-)
-
-export default ErrorMessage
--- src/components/LoadingMessage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const LoadingMessage: React.FC = () => (
-  <h1>Loading...</h1>
-)
-
-export default LoadingMessage
